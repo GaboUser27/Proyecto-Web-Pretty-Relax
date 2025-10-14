fcfadf8e4fc9430827092db4c538fa0adfbdcd34
GaboUser27 <gabriel.rojas.salas@gmail.com>
2025-10-13 23:18:25 -0600

    Blog funciona al filtrar, falta agregar la opcion para etiquetas y demás

--- scriptBlog.js
+++ scriptBlog.js
@@ -1,9 +1,28 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
-const botonesServicios = document.querySelector('.botonesServicios');
-const imagenesServicios = document.querySelector('.imagenesServicios');
+const botonesServiciosC = document.querySelectorAll(".botonesServicios button");
+const imagenesServiciosC = document.querySelectorAll(".imagenesServicios .imagen");
 
-navbarToggle.addEventListener('click', () =>{
+//Listener para ver cual boton está activo en el header
+navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
 });
+
+//La funcion de selección por categoria de las imagenes
+const imagenesFiltro = e => {
+    botonesServiciosC.forEach(btn => btn.classList.remove("active"));
+    e.target.classList.add("active");
+
+    //Iteramos entre imagenes
+    imagenesServiciosC.forEach(imagen => {
+        imagen.classList.add("hide");
+
+        if(imagen.dataset.name === e.target.dataset.name || e.target.dataset.name === "todo"){
+            imagen.classList.remove("hide");
+        }
+    });
+};
+
+//Listener en base a los botones
+botonesServiciosC.forEach(button => button.addEventListener("click", imagenesFiltro));
