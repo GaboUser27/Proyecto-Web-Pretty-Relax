ae2c99efb842bd2ae342c3515d5b5882d0702a17
GaboUser27 <gabriel.rojas.salas@gmail.com>
2025-10-15 23:42:20 -0600

    Nuevo-etiquetas

--- scriptBlog.js
+++ scriptBlog.js
@@ -2,27 +2,34 @@ const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
 const botonesServiciosC = document.querySelectorAll(".botonesServicios button");
 const imagenesServiciosC = document.querySelectorAll(".imagenesServicios .imagen");
+const botonesEtiquetas = document.querySelectorAll(".botonesEtiquetas button");
 
-//Listener para ver cual boton está activo en el header
 navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
 });
 
 //La funcion de selección por categoria de las imagenes
+
 const imagenesFiltro = e => {
     botonesServiciosC.forEach(btn => btn.classList.remove("active"));
     e.target.classList.add("active");
 
-    //Iteramos entre imagenes
+    //Filtrar imágenes
     imagenesServiciosC.forEach(imagen => {
         imagen.classList.add("hide");
-
-        if(imagen.dataset.name === e.target.dataset.name || e.target.dataset.name === "todo"){
+        if (imagen.dataset.name === e.target.dataset.name || e.target.dataset.name === "todo") {
             imagen.classList.remove("hide");
+        }
+    });
+
+    //Filtrar botones de etiquetas
+    botonesEtiquetas.forEach(button => {
+        button.classList.add("hide");
+        if (button.dataset.name === e.target.dataset.name || e.target.dataset.name === "todo") {
+            button.classList.remove("hide");
         }
     });
 };
 
-//Listener en base a los botones
 botonesServiciosC.forEach(button => button.addEventListener("click", imagenesFiltro));
